b0f031093144b21715f61bcf0d69b0143af497ce
chyuhung <977599667@qq.com>
2022-01-14 17:32:21 +0800

    操作申请流程补充

--- 操作流程补充-openpyxl.py
+++ 操作流程补充-openpyxl.py
@@ -15,10 +15,34 @@ def getfilepaths(dirpath,filenames):
         filepaths.append(dirpath+'\\'+filename)
     return filepaths
 
+class WorkBook():
+    def __init__(self,dirpath,filename):
+        self.dirpath=dirpath
+        self.filename=filename
+        self.workbook_path=self.get_workbook_path()
+    def get_workbook_path(self):
+        workbook_path = self.dirpath + '\\' + self.filename
+        return workbook_path
+    def get_workbook_data(self):
+        workbook=openpyxl.load_workbook(self.workbook_path,read_only=True,data_only=True,keep_links=True,keep_vba=True)
+        data=[]
+        datalist=[]
+        for sheet in workbook:
+            for row in sheet.rows:
+                data.append(row)
+            datalist.append(data)
+        return datalist
+
+
 if __name__ == '__main__':
     #文件操作目录
     workdirpath=r'C:\Users\97759\Desktop\project\20220111'
     filenames=filescanner(workdirpath)
     filepaths=getfilepaths(workdirpath,filenames)
-    for filepath in filepaths:
-        print(filepath)
+    for filename in filenames:
+        print('yes')
+        workbook=WorkBook(workdirpath,filename)
+        datalist=workbook.get_workbook_data()
+        for data in datalist:
+            for line in data:
+                print(line)
